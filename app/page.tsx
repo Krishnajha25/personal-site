283f92328215286eb4a37fefc05d4b4d2ce2f676
krishna-jha-25 <krishna@torinit.ca>
2023-07-27 01:48:29 +0530

    added framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,29 @@
 "use client"
 
-import React from 'react'
+import React, { useRef } from 'react'
 import Button from './components/Button'
+import { motion } from 'framer-motion'
 
 const index = () => {
+    const constraintsRef = useRef(null)
+
     return (
-        <div className='w-screen h-screen flex justify-center items-center'>
-            <Button
-                onClick={() => console.log("Button is clicked")}
+        <motion.div ref={constraintsRef} className='w-screen h-screen flex justify-center items-center'>
+            <motion.div
+                drag
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ boxShadow: "0px 0px 15px rgba(0,0,0,0.2)" }}
+                dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+                dragConstraints={constraintsRef}
             >
-                &#128075; Hi There 
-            </Button>
-        </div>
-  )
+                <Button
+                    onClick={() => console.log("Button is clicked")}
+                >
+                    &#128075; Hi There!
+                </Button>
+            </motion.div>
+        </motion.div>
+    )
 }
 
 export default index
